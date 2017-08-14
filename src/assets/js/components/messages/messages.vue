882f08c83b91df95e1fa7dc583c00c44f5ef1be5
EyiLee <sdses9001@gmail.com>
2017-08-14 19:18:11 +0800

    [add] auto hyperlink urls and add user title

--- src/assets/js/components/messages/messages.vue
+++ src/assets/js/components/messages/messages.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="d-flex flex-column">
     <messagesheader></messagesheader>
-    <div class="messages p-3" ref="messages" @scroll="loadMessage">
-      <div v-for="(item, index) in messages" :key="index">
-        <div class="d-flex justify-content-start my-3" :class="[isCurrentUser(item.uid) ? 'flex-row-reverse' : 'flex-row']">
-          <img class="avatar rounded align-self-end" data-toggle="tooltip" v-if="users[item.uid]" :src="users[item.uid].photoURL" :data-original-title="users[item.uid].email">
+    <div class="body p-3" ref="body" @scroll="loadMessage">
+      <div v-for="(item, index) in messages" :key="index" v-if="users[item.uid]">
+        <div :class="[isCurrentUser(item.uid) ? 'user' : 'others']">
+          <img class="avatar rounded align-self-end" data-toggle="tooltip" :src="users[item.uid].photoURL" :data-original-title="users[item.uid].email">
           <div class="mx-3 my-1">
-            <p class="rounded" :class="[isCurrentUser(item.uid) ? 'user' : 'others']" v-for="(text, index) in item.text" :key="index">{{ text }}</p>
+            <span class="title">{{ users[item.uid].email }}</span>
+            <p class="rounded mb-1" :class="[isCurrentUser(item.uid) ? 'user' : 'others']" v-for="(text, index) in item.text" :key="index">
+              <messagestext :text="text"></messagestext>
+            </p>
           </div>
         </div>
       </div>
@@ -17,8 +20,9 @@
 
 <script>
   import firebase from 'firebase'
-
+  import autolinker from 'autolinker'
   import messagesheader from './header'
+  import messagestext from './text'
   import messagesfooter from './footer'
 
   import { mapState, mapGetters } from 'vuex'
@@ -48,7 +52,7 @@
       })
     },
     updated: function () {
-      this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight - this.scrollBottom
+      this.$refs.body.scrollTop = this.$refs.body.scrollHeight - this.scrollBottom
       $('[data-toggle="tooltip"]').tooltip()
     },
     methods: {
@@ -56,29 +60,29 @@
         if (_.size(this.messages) === 0 || _.last(this.messages).uid !== message.uid) {
           this.messages.push({
             uid: message.uid,
-            text: [message.text],
+            text: [autolinker.link(message.text)],
             timestamp: [message.timestamp]
           })
         } else {
-          _.last(this.messages).text.push(message.text)
+          _.last(this.messages).text.push(autolinker.link(message.text))
           _.last(this.messages).timestamp.push(message.timestamp)
         }
-        this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight
+        this.$refs.body.scrollTop = this.$refs.body.scrollHeight
       },
       loadMessage: function () {
-        this.scrollBottom = this.$refs.messages.scrollHeight - this.$refs.messages.scrollTop
-        if (this.$refs.messages.scrollTop === 0) {
+        this.scrollBottom = this.$refs.body.scrollHeight - this.$refs.body.scrollTop
+        if (this.$refs.body.scrollTop === 0) {
           let head = _.head(_.head(this.messages).timestamp) - 1
           firebase.database().ref('/messages/').orderByChild('timestamp').endAt(head).limitToLast(30).once('value').then((snapshot) => {
             _.forEachRight(_.assign(snapshot.val()), (message) => {
               if (_.size(this.messages) === 0 || _.head(this.messages).uid !== message.uid) {
                 this.messages.unshift({
                   uid: message.uid,
-                  text: [message.text],
+                  text: [autolinker.link(message.text)],
                   timestamp: [message.timestamp]
                 })
               } else {
-                _.head(this.messages).text.unshift(message.text)
+                _.head(this.messages).text.unshift(autolinker.link(message.text))
                 _.head(this.messages).timestamp.unshift(message.timestamp)
               }
             })
@@ -89,43 +93,79 @@
         return this.login ? this.user.uid === uid : false
       }
     },
-    components: { messagesheader, messagesfooter }
+    components: { messagesheader, messagestext, messagesfooter }
   }
 </script>
 
-<style>
+<style lang="scss">
   .avatar {
     width: 40px !important;
     height: 40px !important;
+    flex: 0 0 40px;
   }
-  .messages {
+  .body {
+    flex: 0 0 auto;
     height: calc(100vh - 8rem);
-    overflow-y: auto;
+    padding: 1rem !important;
     background-color: white;
+    overflow-y: auto;
   }
-  .messages .user {
-    margin-left: auto;
-    color: white;
-    background-color: #3e9bff;
+  .body .title {
+    display: flex;
+    width: fit-content;
+    font-size: 0.8rem;
+    color: rgba(0, 0, 0, 0.5);
   }
-  .messages .others {
-    margin-right: auto;    
-    background-color: #e8e8e8;
-  }
-  .messages p {
+  .body p {
     width: fit-content;
     min-width: 2rem;
     max-width: 24rem;
     text-align: left;
     white-space: pre-wrap;
     word-break: break-all;
-    color: rgba(0,0,0,0.6);
-    margin-top: 0rem !important;
-    margin-bottom: 0.5rem !important;
-    padding: 0.25rem 0.5rem !important;
+    margin-top: 0;
+    margin-bottom: 0.25rem;
+    padding: 0.25rem 0.5rem;
   }
-  .messages p:last-child {
-    margin-top: 0 !important;
-    margin-bottom: 0 !important;
+  .body p:last-child {
+    margin-bottom: 0;
+  }
+  .user {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-start;
+    margin: 1rem 0 1rem 0;
+    .title {
+      margin-left: auto;
+    }
+    p {
+      margin-left: auto;
+      color: white;
+      background-color: #3e9bff;
+      a {
+        color: white;
+        text-decoration-line: underline;
+      }
+      a:hover {
+        background-color: rgba(255, 255, 255, 0.15);
+      }
+    }
+  }
+  .others {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin: 1rem 0 1rem 0;
+    .title {
+      margin-right: auto;
+    }
+    p {
+      margin-right: auto;
+      color: rgba(0, 0, 0, 0.6);
+      background-color: #e8e8e8;
+      a {
+        color: #3f55ff;
+      }
+    }
   }
 </style>
---
+++ src/assets/js/components/messages/text.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-html="text"></div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'text'
+    ]
+  }
+</script>
+
+<style>
+  
+</style>
